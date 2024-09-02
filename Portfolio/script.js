3bbed00ad026ed3167730bf1c94ac5a1320399ec
lukinhas02 <111981214+lukinhas02@users.noreply.github.com>
2024-09-01 22:38:34 -0300

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,8 @@
+document.getElementById('toggle-mode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        this.textContent = 'Modo Claro';
+    } else {
+        this.textContent = 'Modo Escuro';
+    }
+});
